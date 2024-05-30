713b35839988c28fb6242f38dcfee420afbd707c
Mitheel Ramdaw <106301219+mitheelramdaw@users.noreply.github.com>
2024-05-30 19:56:20 +0200

    use mode.py it is the AI
    model.py is AI
    trained_model.pth - the trained model
    training.py is where the model is made

--- model/verify_tf.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.applications import VGG16
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten, Dropout
-from tensorflow.keras.optimizers import Adam
-
-print("TensorFlow version:", tf.__version__)
-print("Keras version:", tf.keras.__version__)
